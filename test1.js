64f54a64a76daec26c3f142597539f34d087eff7
abdelali20262025 <aythammouabdelali@gmail.com>
2026-04-24 18:12:41 +0100

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,19 @@
+
+let nub1= Number(prompt("entrer 1er nombre :"))
+let nub2= Number(prompt("enter le 2eme nombre"))
+
+
+let somme = nub1 + nub2
+let sust = nub1 - nub2
+let produi = nub1*nub2
+let div = nub1/nub2
+
+console.log("la somme est :",somme)
+console.log("la soustraction est :",sust)
+console.log("le produit est :",produi)
+console.log("la division est :",div)
+
+
+
+
+
